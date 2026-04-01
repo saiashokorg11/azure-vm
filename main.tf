217c2203bf0d342bfb0c3d14a3cd6f50e2c5dece
saigotru <saigotru@gmail.com>
2026-04-01 19:09:18 +0530

    change storage account name

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "tfstate" {
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = var.storage_container_name
-  storage_account_id    = azurerm_storage_account.tfstate.id
+  storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
 
